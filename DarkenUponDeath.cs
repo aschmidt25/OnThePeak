4dd7153d4bf096640918f4d86b39ac2c0cd92d16
aschmidt25 <46943909+aschmidt25@users.noreply.github.com>
2020-10-26 15:01:06 -0500

    Update DarkenUponDeath.cs

--- DarkenUponDeath.cs
+++ DarkenUponDeath.cs
@@ -6,7 +6,6 @@ using UnityEngine.UI;
 
 public class DarkenUponDeath : MonoBehaviour
 {
-    //ColorGrading cg;
     public float lerpRate;
 
     public Image imageToFade;
@@ -22,9 +21,7 @@ public class DarkenUponDeath : MonoBehaviour
     PostProcessProfile darkProfile;
     [SerializeField]
     ColorGrading cgLayer;
-
-    //public Image controlsImage;
-
+    
     [SerializeField]
     private Light[] lights;
 
@@ -36,11 +33,9 @@ public class DarkenUponDeath : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //controlsImage.gameObject.SetActive(false);
-        //cg = this.GetComponent<ColorGrading>();
         volume = this.GetComponent<PostProcessVolume>();
         volume.profile.TryGetSettings(out cgLayer);
-        //imageToFade.gameObject.SetActive(false);
+
         lights = FindObjectsOfType(typeof(Light)) as Light[];
     }
 
@@ -57,63 +52,13 @@ public class DarkenUponDeath : MonoBehaviour
     public void FromLightToDark()
     {
         Debug.Log("Fading to black");
-        //Debug.Log("cgLayer value = " + cgLayer.postExposure.value);
-
-        //controlsImage.gameObject.SetActive(true);
         volume.profile = darkProfile;
-        /*
-        //cgLayer.postExposure.overrideState = true;
-        Mathf.Lerp(currentVal, -15, lerpRate);
-        cgLayer.postExposure.Override(currentVal);
-        cgLayer.postExposure.value = currentVal;
-
-
-
-
-        foreach(Light light in lights)
-        {
-            Mathf.Lerp(light.intensity, 0, 0.05f);
-        }
-
-        */
-
-        //Debug.Log(currentVal);
-        /*if (!imageToFade.gameObject.activeSelf)
-        {
-            imageToFade.gameObject.SetActive(true);
-        }*/
-        //Mathf.Lerp(imageToFade.color.a, 1, lerpRate);
-
-        /*Color newColor = new Color(0, 0, 0, Mathf.Lerp(imageToFade.color.a, 1f, lerpRate));
-        imageToFade.color = newColor;
-        */
-        //if (imageToFade.color.a < 0.95)
-
-        //Debug.Log(imageToFade.color.a);
-        
         
     }
 
     public void FromDarkToLight()
     {
-        //Debug.Log("Fading to transparent");
-        //controlsImage.gameObject.SetActive(false);
         volume.profile = regularProfile;
-        //Mathf.Lerp(cgLayer.postExposure.value, 0, lerpRate);
-
-        //Mathf.Lerp(imageToFade.color.a, 0, lerpRate); 
-
-        /*Color newColor = new Color(0, 0, 0, Mathf.Lerp(imageToFade.color.a, 0f, lerpRate));
-        imageToFade.color = newColor;
-        */
-        //if (imageToFade.color.a <= 0.05)
-        /*
-        if(cgLayer.postExposure.value <= -0.5)
-        {
-            //imageToFade.gameObject.SetActive(false);
-            return false;
-        } else { return true; }
-        */
     }
 
     public IEnumerator DarthFader()
